27dd039cf7c5ce6fe6f9f657c0c4846a1a347273
梨水竹 <46672356+pearwb1235@users.noreply.github.com>
2023-10-03 15:51:38 +0800

    chore: 重新整理身分組資料&身分組

--- src/command/refresh.ts
+++ src/command/refresh.ts
@@ -12,7 +12,10 @@ export default class RefreshCommand implements BaseCommand {
     .setName("refresh")
     .setDescription("立即刷新資料")
     .setDMPermission(false)
-    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator);
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+    .addBooleanOption((option) =>
+      option.setName("role").setDescription("更新身分組")
+    );
   async execute(
     interaction: ChatInputCommandInteraction<CacheType>
   ): Promise<void> {
@@ -21,11 +24,14 @@ export default class RefreshCommand implements BaseCommand {
     });
     const guild = await GuildModel.get(interaction.guildId);
     try {
-      if (await guild.refreshMembers(RefreshMembersType.ALL))
+      if (await guild.refreshMembers(RefreshMembersType.ALL)) {
+        if (interaction.options.getBoolean("role")) {
+          await guild.refreshMemberRoles("重新整理會員身分組(refresh)");
+        }
         await interaction.editReply({
           content: "女僕已整理資料完成",
         });
-      else
+      } else
         await interaction.editReply({
           content: "女僕正在處理其他事情，稍後再來找她吧",
         });
