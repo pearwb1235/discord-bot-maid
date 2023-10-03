b40f5e075d4f23bf5d004fd4dbdf7f4404b51777
梨水竹 <46672356+pearwb1235@users.noreply.github.com>
2023-10-03 15:56:12 +0800

    feat: 處理未有標記身分組的會員

--- src/guild.ts
+++ src/guild.ts
@@ -2,6 +2,39 @@ import { discordClient } from "~/library/discord";
 import { logger } from "~/library/logger";
 import { prismaClient } from "~/library/prisma";
 import { GuildModel } from "~/model/guild";
+import { MemberModel } from "~/model/member";
+
+export async function loadGuilds() {
+  logger.debug(0, "女僕正準備上班...");
+  try {
+    const guilds = await prismaClient.guild.findMany();
+    for (const guild of guilds) {
+      if (!discordClient.guilds.cache.get(guild.guildId)) {
+        logger.error(`女僕遺失了 \`${guild.guildId}\` 的鑰匙`);
+        continue;
+      }
+      if (!guild.markRoleId) continue;
+      const guildModel = await GuildModel.get(guild.guildId);
+      const members = await guildModel
+        .get()
+        .members.fetch()
+        .then((members) => members.toJSON());
+      for (const member of members) {
+        if (member.roles.cache.has(guild.markRoleId)) continue;
+        logger.debug(
+          1,
+          `女僕抓到了下班時間偷溜進來的 \`${member.displayName}(${member.id})\``
+        );
+        const memberModel = await MemberModel.get(guildModel, member.id);
+        await memberModel.freshRoles("加入伺服器(load)");
+      }
+      await guildModel.refreshMembers();
+    }
+    logger.debug(0, "女僕開始上班!");
+  } catch (err) {
+    logger.error("女僕整理資料發生了錯誤:", err.toString());
+  }
+}
 
 export async function refreshGuilds() {
   logger.debug(0, "女僕開始整理資料...");
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import { Events } from "discord.js";
 import { addCommandHandler } from "~/command";
 import { registerEvents } from "~/event";
-import { refreshGuilds } from "~/guild";
+import { loadGuilds, refreshGuilds } from "~/guild";
 import { discordClient } from "~/library/discord";
 import { logger } from "~/library/logger";
 
 discordClient.once(Events.ClientReady, (c) => {
   logger.info(`機器人 \`${c.user.tag}\` 已連線`);
-  refreshGuilds();
+  loadGuilds().then(() => refreshGuilds());
 });
 addCommandHandler(discordClient);
 registerEvents(discordClient);
